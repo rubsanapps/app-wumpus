be1ab7957d1e42fe1e887b8b6042caaec40e2628
Rubén <ruben.arribas@gmail.com>
2020-12-15 14:58:10 +0100

    Eliminado test title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,10 +22,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('app-wumpus');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('app-wumpus app is running!');
-  });
+
 });
